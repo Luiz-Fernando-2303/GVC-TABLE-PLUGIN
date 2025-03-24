6403bcc990731268ecaa401f6cd3c63cd5d330c7
Luiz Fernando <luizferbim@gmail.com>
2025-03-24 11:32:08 -0300

    Simple configuration structure

---
+++ GVC-TABLE-PLUGIN/Components/TemplateConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace GVC_TABLE_PLUGIN.Components
+{
+    internal class TemplateConfiguration
+    {
+        private readonly TemplateConfigurationFunctions functions;
+        public TemplateConfiguration(ExternalCommandData commandData_, List<Object> configList)
+        {
+            functions = new TemplateConfigurationFunctions(commandData_);
+        }
+    }
+
+    public class TemplateConfigurationFunctions
+    {
+        private readonly ExternalCommandData commandData;
+        public TemplateConfigurationFunctions(ExternalCommandData commandData)
+        {
+            this.commandData = commandData;
+        }
+
+        public ICollection<ElementId> GetSelectionIds()
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Selection selection = uiDoc.Selection;
+
+            ICollection<ElementId> selectedIds = selection.GetElementIds();
+            return selectedIds;
+        }
+    }
+
+    public class DataReferenceTable
+    {
+        public List<string> properties = new List<string> { "Area", "Material" };
+        public List<ElementId> Ids = new List<ElementId>();
+        public DataReferenceTable(List<string> properties)
+        {
+
+        }
+    }
+
+}
